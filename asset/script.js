0b4302811c3f6df57b04e05cc68ac8d2ad21d279
18asanket <18asanket@gmail.com>
2018-11-05 16:04:46 +0530

    123

---
+++ asset/script.js
@@ -0,0 +1,51 @@
+// navbar slide down on click
+$(document).on('click', 'a[href^="#"]', function () {
+    $('html').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+    $('.nav-link','a[href^="#"]').addClass('color');
+   
+});
+// navbar slide down on click end
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+		$('.owl-carousel').owlCarousel({
+			loop: true,
+			margin: 20,
+			autoplay:true,
+			dots:false,
+			responsiveClass: true,
+			nav: false,
+			responsive: 
+			{
+				0: 
+				{
+					items: 1,
+				},
+				600: 
+				{
+					items: 1,
+				},
+				1000: 
+				{
+					items: 1
+				}
+			}
+		});
+	});
+
+
+
+
+
+
+ baguetteBox.run('.compact-gallery', { animation: 'slideIn'});
